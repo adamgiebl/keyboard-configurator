ba2da2aa43abc31fe0f42dee307c1655cc944b93
Adam Giebl <adamgiebl@gmail.com>
2021-03-22 20:21:21 +0100

    svg

--- scripts/script.js
+++ scripts/script.js
@@ -14,4 +14,24 @@ async function init() {
   const res = await fetch("./images/Keyboard.svg");
   const svgText = await res.text();
   keyboardContainer.innerHTML = svgText;
+
+  const svg = document.querySelector("#keyboard");
+
+  hideMetalCase(svg);
+
+  const groups = svg.querySelectorAll("g:not(.shadow)");
+
+  groups.forEach((g) => {
+    g.addEventListener("click", () => {
+      g.style.color = "red";
+    });
+  });
+}
+
+init();
+
+function hideMetalCase(svg) {
+  svg.querySelector("#orangecase-shadow").style.display = "none";
+
+  svg.querySelector("#orange-case").style.display = "none";
 }
