26fac0cf8d7cb7c1f783e60e1f86d177502c1cde
Adam Giebl <adamgiebl@gmail.com>
2021-03-23 16:29:13 +0100

    configurator progress

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,39 @@
 
 const keyboardContainer = document.querySelector(".keyboard");
 let options = [];
+
+let selectedOption = null;
+let SVG = null;
+
 const features = {
-  orangeCase: false,
-  blueCase: false,
-  coiledCable: false,
-  straightCable: false,
-  wristRest: false,
+  case: {
+    metalCase: false,
+    plasticCase: false,
+  },
+  cable: {
+    coiledCable: false,
+    straightCable: false,
+  },
+  wristRest: {
+    wristRest: false,
+  },
+};
+
+const colors = {
+  plasticCase: ["blue", "red", "green"],
+  keycaps: ["green", "limegreen", "skyblue"],
+  coiledCable: ["blue", "red", "green"],
+  metalCase: ["blue", "red", "green"],
+  wristRest: ["blue", "red", "green"],
 };
 
 async function init() {
   const res = await fetch("./images/KeyboardMod.svg");
   const svgText = await res.text();
-  keyboardContainer.innerHTML = svgText;
+  keyboardContainer.innerHTML += svgText;
 
   const svg = document.querySelector("#keyboard");
+  SVG = svg;
 
   hideMetalCase(svg);
   hideCable(svg);
@@ -23,21 +42,69 @@ async function init() {
   const groups = svg.querySelectorAll("g:not(.shadow)");
 
   groups.forEach((g) => {
-    //g.style.color = "white";
-  });
-  groups.forEach((g) => {
     g.addEventListener("click", () => {
       g.style.color = "red";
     });
   });
+
+  const colorPicker = document.querySelector(".color-picker");
+
+  const optionItems = document.querySelectorAll(".option-item");
+  optionItems.forEach((item) => {
+    const category = item.dataset.category;
+    const feature = item.dataset.feature;
+    item.addEventListener("click", () => {
+      updateItemState(category, feature);
+
+      if (features[category][feature]) {
+        item.classList.add("active");
+      } else {
+        item.classList.remove("active");
+      }
+
+      console.log(features);
+    });
+  });
 }
 
-init();
+function updateItemState(category, feature) {
+  const optionItems = document.querySelectorAll(".option-item");
+  optionItems.forEach((item) => {
+    if (item.dataset.category === category) {
+      item.classList.remove("active");
+    }
+  });
+  const featuresInCategory = Object.keys(features[category]);
+  featuresInCategory.forEach((featureKey) => {
+    features[category][featureKey] = false;
+  });
+
+  features[category][feature] = !features[category][feature];
+
+  updateSVGState();
+}
+
+function updateSVGState() {
+  const allCategoryKeys = Object.keys(features);
+
+  allCategoryKeys.forEach((categoryKey) => {
+    const allFeatureKeys = Object.keys(features[categoryKey]);
+    allFeatureKeys.forEach((featureKey) => {
+      if (features[categoryKey][featureKey]) {
+        SVG.querySelector(`#${featureKey}`).style.display = "block";
+      } else {
+        SVG.querySelector(`#${featureKey}`).style.display = "none";
+      }
+    });
+  });
+}
 
 function hideMetalCase(svg) {
-  svg.querySelector("#orangecase").style.display = "none";
+  svg.querySelector("#metalCase").style.display = "none";
 }
 
 function hideCable(svg) {
-  svg.querySelector("#straight-cable").style.display = "none";
+  svg.querySelector("#straightCable").style.display = "none";
 }
+
+init();
